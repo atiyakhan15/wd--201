706ee65b285bc7c0a5fe8fef71e8f9a02d625d39
sanket999999999 <125659754+sanket999999999@users.noreply.github.com>
2023-05-24 12:55:14 +0530

    Update app.js

--- todo-app/app.js
+++ todo-app/app.js
@@ -12,7 +12,7 @@ const connectEnsureLogin = require("connect-ensure-login");
 const session = require("express-session");
 const LocalStrategy = require("passport-local");
 const flash = require("connect-flash");
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 
 const saltRounds = 10;
 app.set("views", path.join(__dirname, "views"));
