f97af7aadf40aed5ab818793b4ae5ccc33b50d0a
atiyakhan15 <100838097+atiyakhan15@users.noreply.github.com>
2023-08-23 12:32:10 +0530

    Update index.js

--- http-server/index.js
+++ http-server/index.js
@@ -1,48 +1,52 @@
+//to run through terminal
+//PS C:\Users\GHRCE\WD201\http-server> node index.js --port 5000
+//in explorer http://localhost:5000/
+
 const args = require("minimist")(process.argv.slice(2));
-p = args.port
+port1 = args.port
 console.log(args.port); // prints the value of the --port option
 
 const http = require("http");
 const fs = require("fs");
 
-let homeContent = "";
-let projectContent = "";
-let registrationContent = "";
+let home1 = "";
+let project1 = "";
+let registration1 = "";
 
 fs.readFile("home.html", (err, home) => {
   if (err) {
     throw err;
   }
-  homeContent = home;
+  home1 = home;
 });
 fs.readFile("registration.html", (err, registration) => {
   if (err) {
     throw err;
   }
-  registrationContent = registration;
+  registration1 = registration;
 });
 fs.readFile("project.html", (err, project) => {
   if (err) {
     throw err;
   }
-  projectContent = project;
+  project1 = project;
 });
 http.createServer((request, response) => {
     let url = request.url;
     response.writeHeader(200, { "Content-Type": "text/html" });
     switch (url) {
       case "/project":
-        response.write(projectContent);
+        response.write(project1);
         response.end();
         break;
         case "/registration":
-        response.write(registrationContent);
+        response.write(registration1);
         response.end();
         break;
       default:
-        response.write(homeContent);
+        response.write(home1);
         response.end();
         break;
     }
   })
-  .listen(p);
+  .listen(port1);
